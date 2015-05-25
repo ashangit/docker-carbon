d6591c243f56857f2743722b691f2d83290014f6
nfraison <nfraison@yahoo.fr>
2015-05-25 22:03:29 +0200

    Force file deletion

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 if [ -f /data/carbon/data/$1-a.pid ]
 then
   rm -Rf /data/carbon/data/whisper/carbon/agents/*
-  rm /data/carbon/data/$1-a.pid
+  rm -f /data/carbon/data/$1-a.pid
 fi
 exec "$@"
